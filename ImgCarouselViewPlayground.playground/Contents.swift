f1a2bd38a2dba40d6f90b2770d0f76277704cd0d
kz56cd <kz56cd@gmail.com>
2017-09-29 12:03:57 +0900

    Add drawing view code in playground

--- ImgCarouselViewPlayground.playground/Contents.swift
+++ ImgCarouselViewPlayground.playground/Contents.swift
@@ -1,7 +1,17 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCPlayground
+import PlaygroundSupport
 import ImgCarouselView
 
-var str = "Hello, playground"
+let viewController = UIViewController()
+viewController.view.backgroundColor = .white
 
+let view = UIView()
+view.backgroundColor = .orange
+
+view.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+viewController.view.addSubview(view)
+
+PlaygroundPage.current.liveView = viewController
